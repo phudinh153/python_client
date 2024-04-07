15dfcf3849c8b7412ab30fa937793bbb3fb64ed7
Dinh Xuan Phu <phuxa153@gmail.com>
2024-04-07 21:54:17 +0700

    Initial commit

---
+++ client.js
@@ -0,0 +1,77 @@
+var socketio = require('socket.io-client');
+var pc = null;
+var socket = socketio('http://127.0.0.1:5004'); // replace with your signaling server URL
+
+socket.emit('join', { room: '1', username: 'remote peer' });
+
+function negotiate() {
+    pc.addTransceiver('video', { direction: 'recvonly' });
+    pc.addTransceiver('audio', { direction: 'recvonly' });
+    return pc.createOffer().then((offer) => {
+        return pc.setLocalDescription(offer);
+    }).then(() => {
+        // wait for ICE gathering to complete
+        return new Promise((resolve) => {
+            if (pc.iceGatheringState === 'complete') {
+                resolve();
+            } else {
+                const checkState = () => {
+                    if (pc.iceGatheringState === 'complete') {
+                        pc.removeEventListener('icegatheringstatechange', checkState);
+                        resolve();
+                    }
+                };
+                pc.addEventListener('icegatheringstatechange', checkState);
+            }
+        });
+    }).then(() => {
+        var offer = pc.localDescription;
+        // send the offer to the signaling server
+        socket.emit('offer', {
+            sdp: offer.sdp,
+            type: offer.type,
+        });
+    }).catch((e) => {
+        alert(e);
+    });
+}
+
+// when an answer is received from the signaling server
+socket.on('answer', (answer) => {
+    pc.setRemoteDescription(answer);
+});
+
+
+function start() {
+    var config = {
+        sdpSemantics: 'unified-plan'
+    };
+
+    if (document.getElementById('use-stun').checked) {
+        config.iceServers = [{ urls: ['stun:stun.l.google.com:19302'] }];
+    }
+
+    pc = new RTCPeerConnection(config);
+
+    // connect audio / video
+    pc.addEventListener('track', (evt) => {
+        if (evt.track.kind == 'video') {
+            document.getElementById('video').srcObject = evt.streams[0];
+        } else {
+            document.getElementById('audio').srcObject = evt.streams[0];
+        }
+    });
+
+    document.getElementById('start').style.display = 'none';
+    negotiate();
+    document.getElementById('stop').style.display = 'inline-block';
+}
+
+function stop() {
+    document.getElementById('stop').style.display = 'none';
+
+    // close peer connection
+    setTimeout(() => {
+        pc.close();
+    }, 500);
+}
